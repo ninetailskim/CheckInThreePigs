cda17910cd8d07f0b2c56173c36c97f582e7e579
ninetailskim <ninetailsyeon@163.com>
2020-07-04 18:22:01 +0800

    test

--- GAN/dcgan/DCGAN.py
+++ GAN/dcgan/DCGAN.py
@@ -5,7 +5,7 @@ import math
 
 import paddle
 import paddle.fluid as fluid
-from paddle.fluid.dygraph.nn import Conv2D, Pool2D, Linear, BatchNorm, Conv2DTranspose
+from paddle.fluid.dygraph.nn import Conv2D, Pool2D, Linear, BatchNorm
 
 import shutil
 if os.path.exists("images"):
@@ -34,10 +34,100 @@ class Generator(fluid.dygraph.Layer):
         super(Generator, self).__init__(name_scope)
         self.init_size = opt.img_size // 4
 
-        self.l1 = Linear(opt.latent_dim, 128 * self.init_size ** 2)
-        self.model = []
-        self.model.append(BatchNorm(128))
-        self.model.append(Conv2DTranspose())
+        self.l1 = Linear(opt.latent_dim, 128 * self.init_size ** 2,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)),
+                            bias_attr=ParamAttr(
+                                initializer=fluid.initializer.Constant(0.0))
+                            )
 
-    def forward(self):
-        pass
+        self.bn1 = BatchNorm(128,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02))
+                            )
+        self.conv1 = Conv2D(128, 128, 3, stride=1, padding=1,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)),
+                            bias_attr=ParamAttr(
+                                initializer=fluid.initializer.Constant(0.0))
+                            )
+        self.bn2 = BatchNorm(128,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)))
+        self.conv2 = Conv2D(128, 64, 3, stride=1, padding=1,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)),
+                            bias_attr=ParamAttr(
+                                initializer=fluid.initializer.Constant(0.0))
+                            )
+        self.bn3 = BatchNorm(64,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)))
+        self.conv3 = Conv2D(64, opt.channels, 3, stride=1, padding=1,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)),
+                            bias_attr=ParamAttr(
+                                initializer=fluid.initializer.Constant(0.0))
+                            )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = fluid.layers.reshape(out, shape=[out.shape[0], 128, self.init_size, self.init_size])
+        out = self.bn1(out)
+        out = fluid.layers.image_resize(out, scale=2)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = fluid.layers.leaky_relu(out, alpha=0.2)
+        out = fluid.layers.image_resize(out, scale=2)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = fluid.layers.leaky_relu(out, alpha=0.2)
+        out = self.conv3(out)
+        out = fluid.layers.tanh(out)
+        return out
+
+class DBlock(fluid.dygraph.Layer)；
+    def __init__(self, in_filter, out_filter, bn=True):
+        self.bn = bn
+        self.conv = Conv2D( in_filter, 
+                            out_filter, 3, stride=2, padding=1,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02)),
+                            bias_attr=ParamAttr(
+                                initializer=fluid.initializer.Constant(0.0))
+                            )
+        
+    def forward(self, out):
+        out = self.conv(out)
+        out = fluid.layers.leaky_relu(out)
+        out = fluid.layers.dropout(out, dropout_prob=0.25)
+        if self.bn:
+            out = fluid.layers.batch_norm(out)
+        return out
+
+class Discriminator(fluid.dygraph.Layer):
+    def __init__(self, name_scope):
+        super(Discriminator, self).__init__(name_scope)
+        self.model = []
+        self.model.append(DBlock(opt.channels, 16, bn=False))
+        self.model.append(DBlock(16, 32))
+        self.model.append(DBlock(32, 64))
+        self.model.append(DBlock(64, 128))
+        ds_size = opt.img_size // 2 ** 4
+        self.adv_layer = Linear(128 * ds_size ** 2, 1,  
+                            param_attr=ParamAttr(
+                                initializer=fluid.initializer.Normal(1., 0.02))
+                            )
+
+    def forward(self, out):
+        for layer_ in self.model:
+            out = layer_(out)
+        out = fluid.layers.reshape(out, shape=[out.shape[0], -1])
+        out = self.adv_layer(out)
+        return out
+        
+adversarial_loss = fluid.dygraph.BCELoss()
+
+with fluid.dygraph.guard():
+    generator = Generator("Generator")
+    discriminator = Discriminator("Discriminator")
--- GAN/dcgan/test_conv2dtranspose.py
+++ GAN/dcgan/test_conv2dtranspose.py
@@ -3,6 +3,6 @@ import numpy as np
 
 with fluid.dygraph.guard():
     data = np.random.random((3, 5, 32, 32)).astype('float32')
-    conv2DTranspose = fluid.dygraph.nn.Conv2DTranspose(num_channels=5,num_filters=2, filter_size=33)
-    ret = conv2DTranspose(fluid.dygraph.base.to_variable(data))
-    print(ret.shape)
+    data = fluid.dygraph.to_variable(data)
+    data = fluid.layers.image_resize(data, scale=2)
+    print(data.shape)
